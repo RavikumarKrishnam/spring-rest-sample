d9a90c9a0468c4a210e07438e345bfcee240ae2c
Venkata R Krishnam <kvravi116@gmail.com>
2016-01-31 00:31:46 -0600

    develop

--- src/main/java/com/finance/service/findataprocessor/Application.java
+++ src/main/java/com/finance/service/findataprocessor/Application.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
+
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
